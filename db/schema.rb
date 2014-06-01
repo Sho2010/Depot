f7d37b689926609600f196e54c2d5456c04f3907
sho naito <sho20100@gmail.com>
2014-06-01 09:46:06 +0000

    scaffold line_item

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140601093010) do
+ActiveRecord::Schema.define(version: 20140601094522) do
 
   create_table "carts", force: true do |t|
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "line_items", force: true do |t|
+    t.integer  "product_id"
+    t.integer  "card_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
