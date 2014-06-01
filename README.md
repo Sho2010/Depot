bd683de7da4be05922618cd8f753b8d1dcdfa5bb
sho naito <sho20100@gmail.com>
2014-06-01 13:42:36 +0000

    カートを空にする機能を実装。 Cart#showに合計金額を表示する機能を追加。

--- README.md
+++ README.md
@@ -8,6 +8,29 @@ http://immense-plains-2748.herokuapp.com/
 http://immense-plains-2748.herokuapp.com/products
 
 ## 環境
+
 * Rails 4.1.1
 * ruby 2.1.2p95 (2014-05-08 revision 45877) [x86_64-linux]
 
+## heroku deploy memo
+$ rake assets:precompile
+\#ここで追加されたファイルをgit commitすること、その後にheroku側のmigrateを実行
+$ heroku run rake db:migrate
+
+
+## メモ雑多なこと
+
+### show,editの変更点
+
+おそらくrails3 > 4で
+set_xxxがscoffoldで自動生成されるようになって、before_actionでこいつが呼ばれるようになってる。
+
+だからエラーページ出したくない時とかの処理を上書きする場合
+set_xxxの内容を書き換えてやる。（と、思ったんだけどこれじゃダメかもしれない）
+
+追記：
+http://stackoverflow.com/questions/17541931/template-is-missing-error-even-if-i-have-done-a-redirect-to-in-the-controller
+
+ActiveRecord::RecordNotFoundをset_xxxの中でレスポンスを返しちゃうと
+どうもうまくない事が起きるよと、これどうするのがいいんだろ？
+showの中に書いちゃうのは冗長な気がするし、before_actionから外すのが成功法なのかな？
--- test/functional/products_controller_test.rb
+++ test/functional/products_controller_test.rb
@@ -2,9 +2,9 @@
 #---
 # Excerpted from "Agile Web Development with Rails, 4th Ed.",
 # published by The Pragmatic Bookshelf.
-# Copyrights apply to this code. It may not be used to create training material, 
+# Copyrights apply to this code. It may not be used to create training material,
 # courses, books, articles, and the like. Contact us if you are in doubt.
-# We make no guarantees that this code is fit for any purpose. 
+# We make no guarantees that this code is fit for any purpose.
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #
 # 日本語版については http://ssl.ohmsha.co.jp/cgi-bin/menu.cgi?ISBN=978-4-274-06866-9
@@ -55,7 +55,7 @@ class ProductsControllerTest < ActionController::TestCase
 
   test "should update product" do
     put :update, id: @product.to_param, product: @update
-    assert_redirected_to product_path(assigns(:product))
+    assert_redirected_to action: 'index', notice: 'Product was successfully updated.'
   end
 
   # ...
