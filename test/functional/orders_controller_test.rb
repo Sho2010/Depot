234a736f744a6ef9aec09ded3d55eb9d191bd481
sho naito <sho20100@gmail.com>
2014-06-03 14:47:17 +0000

    heroku deployのためにrake assets:precompile

--- test/functional/orders_controller_test.rb
+++ test/functional/orders_controller_test.rb
@@ -54,7 +54,10 @@ class OrdersControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-
+  test "should update order" do
+    put :update, id: @order.to_param, order: @order.attributes
+    assert_redirected_to order_path(assigns(:order))
+  end
 
   test "should destroy order" do
     assert_difference('Order.count', -1) do
